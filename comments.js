3301e62ff064d20d620b3efc5cc7e515d1166f91
Paul Macaraeg <116135517+PaullyMac@users.noreply.github.com>
2023-09-21 06:45:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,66 @@
-// create web server
+// Create web server
+// Start web server
+// http://localhost:3000
+// http://localhost:3000/about
+// http://localhost:3000/contact
+// http://localhost:3000/help
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-// Array to store comments
-const comments = [];
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
-// Middleware to parse request body
-app.use(express.json());
+// app.get("/about", (req, res) => {
+//   res.send("About page");
+// });
 
-// Route to handle new comments
-app.post('/comments', (req, res) => {
-  const { name, message } = req.body;
-  
-  // Add the comment to the comments array
-  comments.push({ name, message });
-  
-  // Send a success response
-  res.status(201).json({ message: 'Comment added successfully!' });
+// app.get("/contact", (req, res) => {
+//   res.send("Contact page");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.send("Help page");
+// });
+
+// app.get("/help/:topic", (req, res) => {
+//   res.send(`Help page ${req.params.topic}`);
+// });
+
+// app.get("/help/:topic/:subtopic", (req, res) => {
+//   res.send(
+//     `Help page ${req.params.topic} ${req.params.subtopic}`
+//   );
+// });
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
 });
 
-// Route to get all comments
-app.get('/comments', (_req, res) => {
-  res.json(comments);
+app.get("/about", (req, res) => {
+  res.send("<h1>About page</h1>");
 });
 
-// Start the server
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact page</h1>");
+});
+
+app.get("/help", (req, res) => {
+  res.send("<h1>Help page</h1>");
+});
+
+app.get("/help/:topic", (req, res) => {
+  res.send(`<h1>Help page ${req.params.topic}</h1>`);
+});
+
+app.get("/help/:topic/:subtopic", (req, res) => {
+  res.send(
+    `<h1>Help page ${req.params.topic} ${req.params.subtopic}</h1>`
+  );
+});
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
