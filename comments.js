30bb75d8388b472a0dd6f736b9454a7a22a59b26
Paul Macaraeg <116135517+PaullyMac@users.noreply.github.com>
2023-09-21 06:33:30 +0000

    Updated comment web server

--- comments.js
+++ comments.js
@@ -1,11 +1,27 @@
-// Create a web server
-import express from 'express';
+const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define a route for the root URL
-app.get('/', (_req, res) => {
-  res.send('Hello, World!');
+// Array to store comments
+const comments = [];
+
+// Middleware to parse request body
+app.use(express.json());
+
+// Route to handle new comments
+app.post('/comments', (req, res) => {
+  const { name, message } = req.body;
+  
+  // Add the comment to the comments array
+  comments.push({ name, message });
+  
+  // Send a success response
+  res.status(201).json({ message: 'Comment added successfully!' });
+});
+
+// Route to get all comments
+app.get('/comments', (_req, res) => {
+  res.json(comments);
 });
 
 // Start the server
